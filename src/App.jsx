8a3b8060962b7d9e3b5ac50cdd16f163b2f5cf03
WarDog1000 <jhonatansouzameza100@gmail.com>
2023-09-09 21:08:48 -0300

    add getViews function

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,42 @@ import SpamCard from './components/SpamCard';
 import SpamCursor from './components/SpamCursor';
 import SpamContent from './components/SpamContent';
 import scrollReveal from './js/scrollReveal';
+import { useEffect } from 'react';
 
 function App() {
   
   scrollReveal()
 
+  
+  const options = {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      "mode": "no-cors"
+    },
+    body: {
+      "id": 1,
+      "count": 1
+    }
+  };
+
+  const getViews = async () => {
+    try {
+      const res = await fetch("https://views-ik9xt2fxq-jhonysouza100.vercel.app/api");
+      if (!res.ok) {
+        throw new Error(`HTTP error! Status: ${res.status}`);
+      }
+      const data = await res.json();
+      console.log(data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+  
+
+  useEffect(() => {
+    getViews();
+  }, [])
   
 
   return (
